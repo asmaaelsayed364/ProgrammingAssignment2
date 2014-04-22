bcbaa8ac1fb5ee5d7eeebf3a3311c5e58b3f6ce2
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+setwd('C:\Users\Asmaa Hassan\Desktop\R\ProgrammingAssignment2-master')
+## write a pair of functions, namely,"makeCacheMatrix" and "cacheSolve" 
+## both functions will be used to cache the inverse of a matrix
 
-## Write a short comment describing this function
+## "makeCacheMatrix" is a function which creates a special "matrix" object 
+## this function can cache its inverse for the input (which is an invertible square matrix)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  
+  s <- NULL
+  set <- function(z) {
+    x <<- z
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## "cacheSolve" is  a function which computes the inverse of the special "matrix"
+## returned by "makeCacheMatrix " above. 
+## If the inverse has already been calculated, the " cachesolve " should retrieve the
+## inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
